236d6b93093f0b1fcb7dfc65f00f166f4daedb8f
zinzinbin <asdwlstn@naver.com>
2022-06-13 06:10:38 +0000

    update visualize and multigrid

--- src/env/utils/multigrid.py
+++ src/env/utils/multigrid.py
@@ -1,16 +1,36 @@
-'''See https://www.mathematik.uni-wuerzburg.de/fileadmin/10040900/2019/mgintro.pdf
-- multigrid method : use hierarchy of discretizations
-- OpenMG : A new multigrid implementation in python, http://conference.scipy.org/proceedings/scipy2012/pdfs/tom_bertalan.pdf
+'''Multigrid method : use hierarchy of discretizations
+
+the size of matrix should be 2^n+1 for raw and col components
+
+[Functions]
+- restrict : coarsen original matrix[m,n] onto a coarser mesh[m/2,n/2]
+- interpolate : interpolate coarsen matrix[m,n] onto a finer mesh[2m,2n]
+- createVcycle : create V-cycle using recursive-formular to coarsen the matrix
+- smoothJacboi : use simple Jacobi method
+- smoothVcycle : use smoothJacboi to get recursive form of soluation Ax = b 
+* to solve Ax = B, get Ax0 = B1
+* x = x0 + x1, B - Ax0 = B-B1 = Ax1
+* solve Ax1 = B-B1, repeat, B-B1-Ax1 = B2
+* solve Ax2 = B2, repeat
+* x = x0 + x1 + x2 + .... -> smoothVcycle 
+- Bi-CG : Bi-conjugated gradient method, more stable than Jacobi method
+* link : https://towardsdatascience.com/complete-step-by-step-conjugate-gradient-algorithm-from-scratch-202c07fb52a8
+
+[Reference]
+- Introduction to multigrid : https://www.mathematik.uni-wuerzburg.de/fileadmin/10040900/2019/mgintro.pdf
+- OpenMG, A new multigrid implementation in python : http://conference.scipy.org/proceedings/scipy2012/pdfs/tom_bertalan.pdf
+- Geometric Multigrid Algorithm : http://feflow.info/uploads/media/Stueben.pdf
 '''
 import numpy as np
 from scipy.sparse.linalg import factorized
 from scipy.sparse import eye
+from scipy.sparse import csr_matrix
 from typing import Optional, Union, Tuple, List
 
-def restrict(origin, out = None, avg = False):
+def restrict(origin:np.ndarray, out:Optional[np.ndarray] = None, avg :bool= False):
     ''' coarsen the original matrix onto a coarser mesh
     inputs : origin (nx x ny 2D array)
-    '''
+    ''' 
 
     nx = origin.shape[0]
     ny = origin.shape[1]
@@ -44,7 +64,7 @@ def restrict(origin, out = None, avg = False):
 
     return out
 
-def interpolate(origin, out = None):
+def interpolate(origin:np.ndarray, out :Optional[np.ndarray]= None):
     '''Interpolate a solution onto a finer mesh
     '''
 
@@ -99,7 +119,7 @@ class MultigridJacobi:
         self.sub_b = None
         self.x_update = None
     
-    def __call__(self, xi, bi, n_cycle = None, n_iter = None):
+    def __call__(self, xi, bi, n_cycle :Optional[int]= None, n_iter :Optional[int]= None):
         x = np.reshape(xi, -1)
         b = np.reshape(bi, -1)
 
@@ -138,7 +158,7 @@ def createVcycle(nx : int, ny : int, generator, n_levels : int = 4, n_cycle : in
         nxsub = (nx - 1) // 2 + 1
         nysub = (ny - 1) // 2 + 1
 
-        subsolver = createVcycle(nxsub, nysub, generator, n_levels - 1, n_iter, direct)
+        subsolver = createVcycle(nxsub, nysub, generator, n_levels - 1, n_cycle = 1, n_iter = n_iter, direct = direct)
 
         A = generator(nx, ny)
 
@@ -154,7 +174,6 @@ def createVcycle(nx : int, ny : int, generator, n_levels : int = 4, n_cycle : in
 def smoothJacobi(A, x:Union[np.ndarray, np.array], b:Union[np.ndarray, np.array], dx : float, dy:float):
     '''smooth the solution using jacobi method
     '''
-
     if b.shape != x.shape:
         raise ValueError("b and x have different shapes")
     
@@ -162,9 +181,59 @@ def smoothJacobi(A, x:Union[np.ndarray, np.array], b:Union[np.ndarray, np.array]
 
     return smooth
 
-def smoothVcycle(A,x,b,dx,dy,n_iter = 10, sublevels = 0, direct = True):
+def CGA(A, x:Union[np.ndarray, np.array], b:Union[np.ndarray, np.array], dx : float, dy:float):
+    '''### Conjugate Gradients Algorithm
+    argument : A(LaplaceOp), x : np.ndarray, b : np.ndarray, dx : float, dy : float
+    
+    process
+    1. initialize x0
+    2. initialize residual r = b - Ax0
+    3. initialize v1 = r0
+    4. iterate process
+        - Line search : alpha_k = v_k.T * r_{k-1} / v_k.T * A * v_k
+        - update estimate : x_k = x_{k-1} + alpha_k * v_k
+        - update residual : r_k = b - Ax_{k-1}
+        - direction update : v_k = r_{k-1} - r_{k-1}*A*v_{k-1} / (v_{k-1} * A * v_{k-1}) * v_{k-1}
+    '''
+    if b.shape != x.shape:
+        raise ValueError("b and x have different shapes")
+
+    # initialize x0
+    x_prev = np.zeros_like(x)
+    r_prev = b - A(x_prev,dx,dy)
+
+    # initialize v0 
+    # v : A-conjugated vector set v1,v2,v3,....
+    v_curr = r_prev
+
+    n = x.shape[0] 
+
+    for iter in range(n):
+        
+        # line search
+        alpha = v_curr.T @ r_prev
+        alpha /= np.dot(v_curr, A(v_curr, dx, dy) @ v_curr)
+
+        # update estimate
+        x_curr = x_prev + alpha * v_curr
+        
+        # update residual
+        r_curr = b - A(x_prev, dx, dy)
+
+        # update previous direction
+        v_prev = v_curr
+        r_prev = r_curr
+        x_prev = x_curr
+
+        # direction update
+        v_curr = r_curr - r_curr @ A(v_prev, dx, dy) / np.dot(v_prev @ A(v_prev, dx, dy), v_prev)
+
+    return x_curr
+
+def smoothVcycle(A,x,b,dx,dy, n_iter:int= 10, sublevels:int= 0, direct:bool= True):
     for i in range(n_iter):
         x = smoothJacobi(A,x,b,dx,dy)
+        #x = CGA(A,x,b,dx,dy)
     
     if sublevels > 0:
         error = b - A(x,dx,dy)
@@ -180,6 +249,7 @@ def smoothVcycle(A,x,b,dx,dy,n_iter = 10, sublevels = 0, direct = True):
     
     for i in range(n_iter):
         x = smoothJacobi(A,x,b,dx,dy)
+        #x = CGA(A,x,b,dx,dy)
     
     return x
 
@@ -200,7 +270,7 @@ class LaplacianOp:
     for use with the multigrid solver
     """
 
-    def __call__(self, f, dx, dy):
+    def __call__(self, f:np.ndarray, dx:float, dy:float)->np.ndarray:
         nx = f.shape[0]
         ny = f.shape[1]
 
@@ -216,16 +286,16 @@ class LaplacianOp:
 
         return b
 
-    def diag(self, dx, dy):
+    def diag(self, dx:float, dy:float):
         return -2.0 / dx ** 2 - 2.0 / dy ** 2
 
 
 class LaplaceSparse:
-    def __init__(self, Lx, Ly):
+    def __init__(self, Lx:float, Ly:float):
         self.Lx = Lx
         self.Ly = Ly
 
-    def __call__(self, nx, ny):
+    def __call__(self, nx:int, ny:int)->csr_matrix:
         dx = self.Lx / (nx - 1)
         dy = self.Ly / (ny - 1)
 
@@ -251,23 +321,22 @@ class LaplaceSparse:
         # Convert to Compressed Sparse Row (CSR) format
         return A.tocsr()
 
-
 if __name__ == "__main__":
 
     # Test case
 
-    from numpy import meshgrid, exp, linspace
+    from numpy import exp, linspace
     import matplotlib.pyplot as plt
 
     from timeit import default_timer as timer
 
-    nx = 65
-    ny = 65
+    nx = 129
+    ny = 129
 
     dx = 1.0 / (nx - 1)
     dy = 1.0 / (ny - 1)
 
-    xx, yy = meshgrid(linspace(0, 1, nx), linspace(0, 1, ny))
+    xx, yy = np.meshgrid(linspace(0, 1, nx), linspace(0, 1, ny))
 
     rhs = exp(-((xx - 0.5) ** 2 + (yy - 0.5) ** 2) / 0.4 ** 2)
 
@@ -283,6 +352,7 @@ if __name__ == "__main__":
     A = LaplacianOp()
 
     ################ SIMPLE ITERATIVE SOLVER ##############
+    print("\n##### simple iterative solver : jacobi method #####\n")
 
     for i in range(1):
         x2 = smoothJacobi(A, x, rhs, dx, dy)
@@ -292,21 +362,21 @@ if __name__ == "__main__":
 
     ################ MULTIGRID SOLVER #######################
 
-    print("Python multigrid solver")
+    print("\n##### multigrid solver #####\n")
 
     x = np.zeros([nx, ny])
 
     start = timer()
-    x = smoothMG(A, x, rhs, dx, dy, n_iter=5, sublevels=3, n_cycle=2)
+    x = smoothMG(A, x, rhs, dx, dy, n_iter=4, sublevels=3, n_cycle=16)
     end = timer()
 
     error = rhs - A(x, dx, dy)
     print("Max error : {0}".format(np.max(abs(error))))
-    print("Run time  : {0} seconds".format(end - start))
+    print("Run time  : {0}(s)".format(end - start))
 
     ################ SPARSE MATRIX ##########################
 
-    print("Sparse matrix solver")
+    print("\n##### Sparse matrix solver #####\n")
 
     x2 = np.zeros([nx, ny])
 
@@ -323,12 +393,12 @@ if __name__ == "__main__":
     error = rhs - A(x2, dx, dy)
     print("Max error : {0}".format(np.max(abs(error))))
     print(
-        "Setup time: {0}, run time: {1} seconds".format(
+        "Setup time: {0}(s)\nRun time: {1}(s)".format(
             start_solve - start, end - start_solve
         )
     )
 
-    print("Values: {0}, {1}".format(x2[10, 20], x[10, 20]))
+    print("Values: {0}, {1}\n".format(x2[10, 20], x[10, 20]))
 
     f = plt.figure()
     # plt.contourf(x)
