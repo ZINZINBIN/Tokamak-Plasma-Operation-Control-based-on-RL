ee99e7d4e7405e9c867a6472c3ad16a9989b285e
zinzinbin <asdwlstn@naver.com>
2023-03-20 14:52:06 +0900

    update play_sac.py code

--- src/rl/sac.py
+++ src/rl/sac.py
@@ -398,3 +398,69 @@ def train_sac(
     env.close()
 
     return episode_durations, reward_list
+
+
+def evaluate_sac(
+    env : NeuralEnv,
+    init_generator : InitGenerator,
+    policy_network : GaussianPolicy, 
+    device : Optional[str] = "cpu",
+    shot_num : Optional[int] = None
+    ):
+    
+    policy_network.eval()
+
+    if device is None:
+        device = "cpu"
+
+    episode_durations = []
+    reward_list = []
+    state_list = []
+    action_list = []
+
+    if shot_num:
+        init_generator.shot_num = shot_num
+        
+    # update new initial state and action
+    init_state, init_action = init_generator.get_state()
+    env.update_init_state(init_state, init_action)
+    
+    # reset ou noise and current state from env
+    state = env.reset()
+    state_list.append(env.get_state().squeeze(0))
+    action_list.append(env.get_action().squeeze(0))
+
+    for t in count():
+        policy_network.eval()
+        action, _, _ = policy_network.sample(state.to(device))
+        action = action.detach().cpu()             
+        _, reward, done, _ = env.step(action)
+
+        if state.ndim == 3:
+            state = state.squeeze(0)
+        
+        if action.ndim == 3:
+            action = action.squeeze(0)
+        
+        reward_list.append(reward.detach().cpu().numpy())
+        state_list.append(state.detach().cpu().numpy())
+        action_list.append(action.detach().cpu().numpy())
+        
+        if not done:
+            next_state = env.get_state()
+            state = next_state
+        else:
+            next_state = None
+            break
+
+    # memory cache delete
+    gc.collect()
+
+    # torch cache delete
+    torch.cuda.empty_cache()
+
+    print("\nTokamak Plasma Operation Evaluation Process done..!")
+    env.close()
+    
+    return state_list, action_list, reward_list
+    
